91bc408e68711b689fb06652f63ffbd4429c47fe
nasta987 <99967637+nasta987@users.noreply.github.com>
2024-08-23 19:19:18 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ let arr = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let bracketStr = str[i];
+    
+   
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      let openBracket = bracketsConfig[j][0];
+      let closeBracket = bracketsConfig[j][1];
+      
+    
+      if (bracketStr === openBracket) {
+    
+        if (openBracket === closeBracket) {
+        
+          if (arr[arr.length - 1] === openBracket) {
+            arr.pop();
+          } else {
+            arr.push(bracketStr);
+          }
+        } else {
+          arr.push(bracketStr);
+        }
+      }
+    
+      else if (bracketStr === closeBracket) {
+      
+        if (arr.length === 0 || arr.pop() !== openBracket) {
+          return false;
+        }
+      }
+    }
+  }
+
+  return arr.length === 0;
 }
